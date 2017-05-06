939c77acb3564a0c8a4419d8ca6492280d86ad41
IvanMykhayliv <ivanmykhayliv@yahoo.com>
2017-05-05 23:43:49 -0400

    Del

--- ChessProject/ChessProject/ChessProject/ChessColor.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHESSCOLOR_H
-#define CHESSCOLOR_H
-
-namespace IvanChessGame
-{
-	enum ChessColor { WHITE=0, BLACK=1 };
-}
-
-#endif /* CHESSCOLOR_H */
--- ChessProject/ChessProject/ChessProject/ErrorMove.cpp
+++
@@ -1,7 +0,0 @@
-#include "ErrorMove.h"
-using namespace std;
-using namespace IvanChessGame;
-
-ErrorMove::ErrorMove(string strErrorMessage) { _strErrorMessage = strErrorMessage; }
-ErrorMove::~ErrorMove(){}
-string ErrorMove::GetErrorMessage (){ return _strErrorMessage; }
